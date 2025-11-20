b3fe7cbb4b2a801b622119e22e35d31f13661f85
Austin <austin.a.guevara@gmail.com>
2025-11-19 20:45:46 -0800

    added basic bmp585 code to validate

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gap.h>
 #include <zephyr/bluetooth/conn.h>
+#include <zephyr/drivers/sensor.h>
 
 #include "ble_manager.h"
 
@@ -27,6 +28,79 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 #define ACCEL_NODE DT_ALIAS(accel0)
 #define PRESS_NODE DT_ALIAS(press0)
+
+#define BMP5_SEA_LEVEL_PRESSURE_PA 101325
+/* ODR settings */
+#define BMP5_ODR_240_HZ 0x00
+#define BMP5_ODR_218_5_HZ 0x01
+#define BMP5_ODR_199_1_HZ 0x02
+#define BMP5_ODR_179_2_HZ 0x03
+#define BMP5_ODR_160_HZ 0x04
+#define BMP5_ODR_149_3_HZ 0x05
+#define BMP5_ODR_140_HZ 0x06
+#define BMP5_ODR_129_8_HZ 0x07
+#define BMP5_ODR_120_HZ 0x08
+#define BMP5_ODR_110_1_HZ 0x09
+#define BMP5_ODR_100_2_HZ 0x0A
+#define BMP5_ODR_89_6_HZ 0x0B
+#define BMP5_ODR_80_HZ 0x0C
+#define BMP5_ODR_70_HZ 0x0D
+#define BMP5_ODR_60_HZ 0x0E
+#define BMP5_ODR_50_HZ 0x0F
+#define BMP5_ODR_45_HZ 0x10
+#define BMP5_ODR_40_HZ 0x11
+#define BMP5_ODR_35_HZ 0x12
+#define BMP5_ODR_30_HZ 0x13
+#define BMP5_ODR_25_HZ 0x14
+#define BMP5_ODR_20_HZ 0x15
+#define BMP5_ODR_15_HZ 0x16
+#define BMP5_ODR_10_HZ 0x17
+#define BMP5_ODR_05_HZ 0x18
+#define BMP5_ODR_04_HZ 0x19
+#define BMP5_ODR_03_HZ 0x1A
+#define BMP5_ODR_02_HZ 0x1B
+#define BMP5_ODR_01_HZ 0x1C
+#define BMP5_ODR_0_5_HZ 0x1D
+#define BMP5_ODR_0_250_HZ 0x1E
+#define BMP5_ODR_0_125_HZ 0x1F
+
+/* Oversampling for temperature and pressure */
+#define BMP5_OVERSAMPLING_1X 0x00
+#define BMP5_OVERSAMPLING_2X 0x01
+#define BMP5_OVERSAMPLING_4X 0x02
+#define BMP5_OVERSAMPLING_8X 0x03
+#define BMP5_OVERSAMPLING_16X 0x04
+#define BMP5_OVERSAMPLING_32X 0x05
+#define BMP5_OVERSAMPLING_64X 0x06
+#define BMP5_OVERSAMPLING_128X 0x07
+
+/* IIR filter for temperature and pressure */
+#define BMP5_IIR_FILTER_BYPASS 0x00
+#define BMP5_IIR_FILTER_COEFF_1 0x01
+#define BMP5_IIR_FILTER_COEFF_3 0x02
+#define BMP5_IIR_FILTER_COEFF_7 0x03
+#define BMP5_IIR_FILTER_COEFF_15 0x04
+#define BMP5_IIR_FILTER_COEFF_31 0x05
+#define BMP5_IIR_FILTER_COEFF_63 0x06
+#define BMP5_IIR_FILTER_COEFF_127 0x07
+
+/* This is used to enable IIR config, keep in mind that disabling IIR back in runtime is not supported yet */
+#define BMP5_ATTR_IIR_CONFIG (SENSOR_ATTR_PRIV_START + 1u)
+#define BMP5_ATTR_POWER_MODE (SENSOR_ATTR_PRIV_START + 2u)
+#define BMP5_ATTR_ALTITUDE (SENSOR_ATTR_PRIV_START + 3u)
+enum bmp5_powermode
+{
+	/*! Standby powermode */
+	BMP5_POWERMODE_STANDBY,
+	/*! Normal powermode */
+	BMP5_POWERMODE_NORMAL,
+	/*! Forced powermode */
+	BMP5_POWERMODE_FORCED,
+	/*! Continous powermode */
+	BMP5_POWERMODE_CONTINOUS,
+	/*! Deep standby powermode */
+	BMP5_POWERMODE_DEEP_STANDBY
+};
 
 /* Pull out the device from the node */
 static const struct gpio_dt_spec led0_spec = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -95,6 +169,13 @@ int setup_gpio(void)
 	gpio_init_callback(&button_cb_data, button_pressed, BIT(button0_spec.pin));
 	gpio_add_callback(button0_spec.port, &button_cb_data);
 
+	if (!device_is_ready(press_dev))
+	{
+		LOG_INF("Pressure sensor device not ready\n");
+		return -ENODEV;
+	}
+	LOG_INF("Pressure sensor device: %s ready=%d\n", press_dev->name, device_is_ready(press_dev));
+
 	if (!device_is_ready(accel_dev))
 	{
 		LOG_INF("Accelerometer device not ready\n");
@@ -102,13 +183,6 @@ int setup_gpio(void)
 		return 0; // temp workaround while debugging SPI issues
 	}
 	LOG_INF("Accelerometer device: %s ready=%d\n", accel_dev->name, device_is_ready(accel_dev));
-
-	if (!device_is_ready(press_dev))
-	{
-		LOG_INF("Pressure sensor device not ready\n");
-		return -ENODEV;
-	}
-	LOG_INF("Pressure sensor device: %s ready=%d\n", press_dev->name, device_is_ready(press_dev));
 
 	return 0;
 }
@@ -128,15 +202,56 @@ int main(void)
 	gpio_pin_set_dt(&led1_spec, 0);
 
 	err = ble_manager_init();
+
 	if (err)
 	{
 		LOG_ERR("BLE Manager init failed (err %d)\n", err);
 		return -1;
 	}
 
+	struct sensor_value pressure_oversampling_rate = {BMP5_OVERSAMPLING_16X, 1};
+	if (sensor_attr_set(press_dev, SENSOR_CHAN_PRESS, SENSOR_ATTR_OVERSAMPLING, &pressure_oversampling_rate) != 0)
+	{
+		printf("Could not set pressure oversampling rate of %d", pressure_oversampling_rate.val1);
+		return 0;
+	}
+	struct sensor_value temperature_oversampling_rate = {BMP5_OVERSAMPLING_2X, 1};
+	if (sensor_attr_set(press_dev, SENSOR_CHAN_AMBIENT_TEMP, SENSOR_ATTR_OVERSAMPLING, &temperature_oversampling_rate) != 0)
+	{
+		printf("Could not set temperature oversampling rate of %d", pressure_oversampling_rate.val1);
+		return 0;
+	}
+
+	struct sensor_value pressure_odr = {BMP5_ODR_25_HZ, 0};
+	if (sensor_attr_set(press_dev, SENSOR_CHAN_AMBIENT_TEMP, SENSOR_ATTR_SAMPLING_FREQUENCY, &pressure_odr) != 0)
+	{
+		printf("Could not set pressure odr of %d", pressure_oversampling_rate.val1);
+		return 0;
+	}
+
+	struct sensor_value power_mode = {BMP5_POWERMODE_NORMAL, 0};
+	if (sensor_attr_set(press_dev, SENSOR_CHAN_PRESS, BMP5_ATTR_POWER_MODE, &power_mode) != 0)
+	{
+		printf("Could not set powermode  of %d", power_mode.val1);
+		return 0;
+	}
+
+	k_msleep(1000);
+	printf("Starting pressure& temperature polling.\n");
+	struct sensor_value pressure;
+	struct sensor_value temperature;
+
 	while (1)
 	{
 		k_msleep(1000);
+		if (sensor_sample_fetch_chan(press_dev, SENSOR_CHAN_ALL) == 0)
+		{
+			sensor_channel_get(press_dev, SENSOR_CHAN_PRESS, &pressure);
+			sensor_channel_get(press_dev, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
+
+			printf("temp %f Cel, pressure %f kPa \n",
+				   sensor_value_to_double(&temperature), sensor_value_to_double(&pressure));
+		}
 	}
 
 	return 0;
